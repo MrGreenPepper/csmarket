e3843bd1593a530c15a3a33383595b12b4536153
MrGreenPepper <sebastian_truemper@posteo.de>
2023-07-07 14:00:28 +0200

    parses, splits & fixes the historic data

--- scrapper/steam/getStatistics.js
+++ scrapper/steam/getStatistics.js
@@ -29,6 +29,90 @@ export async function getAll() {
 		let historicData = await dbHandler
 			.sqlQuery(sqlSyntaxes.loadItemContent, [currentName])
 			.then((res) => res.rows[0].historicdata);
+		let [dateArray, priceArray, countArray] = splitIntoSingleArrays(historicData);
 		console.log('test');
+		/**wanted statistcs
+		 *
+		 */
+		//lifetime container
+		//elastizität menge price
+		//martvolumen
+		//handelsvolumen
+		//relation markt zu handelsvolumen
+		//.. bräuchte die angebots und nachfrage daten
 	}
+}
+
+/**Parses the raw historic data from db into obj and splits them into separated arrays.
+ * Also fixes missing days by approximating them
+ * @param {array} 	historicData 	- 	the array containing every day per entry as single string
+ * @param {boolean}	approximateMissingDays 	- default = true, the historic data is incomplete, instead of take care later in every single step we approximate the missing days
+ * @returns {[dates:array, prices:array, count:arrayx]} [dateArray, priceArray, countArray]		-	the parsed, splitted and cleaned data
+ */
+function splitIntoSingleArrays(historicData, aproximateMissingDays = true) {
+	let dateArray = [];
+	let priceArray = [];
+	let countArray = [];
+	let dateRegex = /[a-z,A-Z]{3}.[0-9]{2}.[0-9]{4}/gim;
+	let dayRegex = / [0-9]{2} /gim;
+	let yearRegex = /[0-9]{4}/gim;
+	let monthRegex = /[a-z,A-Z]{3}/gim;
+
+	//split into arrays
+	historicData.forEach((entry) => {
+		entry = JSON.parse(entry);
+		dateArray.push(entry.date);
+		priceArray.push(entry.price);
+		countArray.push(entry.count);
+	});
+	//clean the dates
+	dateArray = dateArray.map((date) => {
+		date = date.match(dateRegex)[0];
+		let day = date.match(dayRegex)[0].trim();
+		day = parseInt(day);
+		let month = date.match(monthRegex)[0];
+		let year = date.match(yearRegex)[0];
+		year = parseInt(year);
+		return { day: day, month: month, year: year };
+	});
+	//tests the data
+	if (dateArray.length == priceArray.length && dateArray.length == countArray.length) {
+	} else throw console.error('incomplete historic data');
+	//approximate missing dates
+	[dateArray, priceArray, countArray] = approximateMissingDays([dateArray, priceArray, countArray]);
+
+	return [dateArray, priceArray, countArray];
+}
+
+function approximateMissingDays([dateArray, priceArray, countArray]) {
+	let fixedDateArray = [];
+	let fixedPriceArray = [];
+	let fixedCountArray = [];
+	//find missing days
+	for (let i = 0; i < dateArray.length - 1; i++) {
+		//first push origin
+		fixedDateArray.push(dateArray[i]);
+		fixedPriceArray.push(priceArray[i]);
+		fixedCountArray.push(countArray[i]);
+		//TODO correct month lengths (the last months had realy end)
+		//TODO: think about approximating the price like that (peak days) ... maybe missing days before peak days
+		if (dateArray[i].day + 1 != dateArray[i + 1].day && dateArray[i].month == dateArray[i + 1].month) {
+			//count missing days
+			let missingDays = dateArray[i + 1].day - dateArray[i].day - 1;
+			let countChange = countArray[i + 1] - countArray[i];
+			let countChangePerDay = countChange / missingDays;
+			let priceChange = priceArray[i + 1] - priceArray[i];
+			let priceChangePerDay = priceChange / missingDays;
+			for (let n = 1; n <= missingDays; n++) {
+				let currentDay = dateArray[i].day + n;
+				let currentDate = { day: currentDay, month: dateArray[i].month, year: dateArray[i].year };
+				let currentPrice = priceArray[i] + n * priceChangePerDay;
+				let currentCount = countArray[i] + n * countChangePerDay;
+				fixedDateArray.push(currentDate);
+				fixedPriceArray.push(currentPrice);
+				fixedCountArray.push(currentCount);
+			}
+		}
+	}
+	return [fixedDateArray, fixedPriceArray, fixedCountArray];
 }
